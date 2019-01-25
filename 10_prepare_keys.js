5f7923b7abdf7bed6baad5e24db317fc6efcec4f
Marcio Galli <taboca@gmail.com>
2019-01-25 16:25:42 -0200

    Testing

--- 10_prepare_keys.js
+++ 10_prepare_keys.js
@@ -5,7 +5,9 @@ console.log("----------------");
 console.log("These are keys, private and public. ");
 console.log("The public key will be sent to the browser JS code before creating a subscription. ")
 console.log("Only the public key will be eventually sent to the the client code. ");
+
 let JSONconfig = webPush.generateVAPIDKeys();
+
 console.log("Copy the following key file config as 15_vapid_keys.json:");
 console.log("----------------");
 console.log(stringify(JSONconfig));
