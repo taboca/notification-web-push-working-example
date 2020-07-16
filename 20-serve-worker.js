823316f296ae05c1e7e6ff995032c0a63f8512eb
Marcio Galli <taboca@gmail.com>
2020-07-16 15:59:17 -0300

    Simple html serving

--- 20-serve-worker.js
+++ 20-serve-worker.js
@@ -3,16 +3,7 @@ const path       = require('path');
 var bodyParser   = require('body-parser');
 const app        = express();
 
-const configKeys = require('./15_vapid_keys.json');
-
 app.use(bodyParser.json());
-
-app.get('/test/vapidPublicKey', function(req, res) {
-  res.send(configKeys['publicKey']);
-  console.log("Returned the public key:");
-  console.log(configKeys['publicKey']);
-  console.log("MANUAL INTERVENTION - stop this script and run 30-serve-register!")
-});
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
