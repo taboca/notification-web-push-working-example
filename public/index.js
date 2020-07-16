aa81dd9a10f10bc50633493c9391cc030a257605
Marcio Galli <taboca@gmail.com>
2020-07-16 15:55:52 -0300

    Just sending the worker

--- public/index.js
+++ public/index.js
@@ -4,38 +4,8 @@ navigator.serviceWorker.register('service-worker.js');
 navigator.serviceWorker.ready
 .then(function(registration) {
   // Use the PushManager to get the user's subscription to the push service.
-  return registration.pushManager.getSubscription()
-  .then(async function(subscription) {
-    // If a subscription was found, return it.
-    if (subscription) {
-      return subscription;
-    }
-
-    // Get the server's public key
-    const response = await fetch('./test/vapidPublicKey');
-    const vapidPublicKey = await response.text();
-    // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
-    // urlBase64ToUint8Array() is defined in /tools.js
-    const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
-
-    // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
-    // send notifications that don't have a visible effect for the user).
-    return registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: convertedVapidKey
-    });
-  });
+  console.log("This worker is running...")
 }).then(function(subscription) {
   // Send the subscription details to the server using the Fetch API.
-  console.log("Calling the server to register the subscription...");
-  fetch('./test/register', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      subscription: subscription
-    }),
-  });
-
+  console.log("Something else..");
 });
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,3 @@
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
-  // Keep the service worker alive until the notification is created.
-  event.waitUntil(
-    // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
-    self.registration.showNotification('ServiceWorker Cookbook', {
-      body: 'Olá mundo!',
-    })
-  );
 });
