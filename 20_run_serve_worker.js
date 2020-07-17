ab1ad7ba114345e5ea6a9de5814e953673f6e4e8
Marcio Galli <taboca@gmail.com>
2020-07-17 15:58:23 -0300

    Endpoint for serving the key

--- 20_run_serve_worker.js
+++ 20_run_serve_worker.js
@@ -3,7 +3,16 @@ const path       = require('path');
 var bodyParser   = require('body-parser');
 const app        = express();
 
+const configKeys = require('./10_data_vapid_keys.json');
+
 app.use(bodyParser.json());
+
+app.get('/test/vapidPublicKey', function(req, res) {
+  res.send(configKeys['publicKey']);
+  console.log("Returned the public key:");
+  console.log(configKeys['publicKey']);
+  console.log("MANUAL INTERVENTION - stop this script and run 30-serve-register!")
+});
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
