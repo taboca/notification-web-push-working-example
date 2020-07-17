daff549d933cfa35a607325e81edd163f4bad3f9
Marcio Galli <taboca@gmail.com>
2020-07-17 15:59:39 -0300

    Getting the keys from the server and subscribing

--- public/index.js
+++ public/index.js
@@ -11,5 +11,25 @@ navigator.serviceWorker.register('service-worker.js');
 navigator.serviceWorker.ready
 .then(function(registration) {
   // Use the PushManager to get the user's subscription to the push service.
-  console.log("This worker is running...")
+  console.log("This worker is running...");
+  
+  // If a subscription was found, return it.
+  if (subscription) {
+    return subscription;
+  }
+
+  // Get the server's public key
+  const response = await fetch('./test/vapidPublicKey');
+  const vapidPublicKey = await response.text();
+  // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
+  // urlBase64ToUint8Array() is defined in /tools.js
+  const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+
+  // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+  // send notifications that don't have a visible effect for the user).
+  return registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: convertedVapidKey
+  });
+  
 });
