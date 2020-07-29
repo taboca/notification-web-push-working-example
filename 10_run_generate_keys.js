c2c99195855fc4e22fa3fab20c14edae42ed73d7
Marcio Galli <taboca@gmail.com>
2020-07-29 14:39:03 -0300

    Now the script saves the file

--- 10_run_generate_keys.js
+++ 10_run_generate_keys.js
@@ -1,15 +1,20 @@
+const fs = require('fs');
 const webPush = require('web-push');
 var stringify = require('json-stringify-pretty-compact');
 
 console.log("----------------");
-console.log("These are keys, private and public. ");
+console.log("We need to have keys, private and public. ");
 console.log("The public key will be sent to the browser JS code before creating a subscription. ")
 console.log("Only the public key will be eventually sent to the the client code. ");
 
 let JSONconfig = webPush.generateVAPIDKeys();
 
-console.log("Copy the following key file config as 10_data_vapid_keys.json:");
+console.log("The following are the keys that I have been generating. Now I am done with that. ");
 console.log("----------------");
 console.log(stringify(JSONconfig));
 console.log("----------------");
 
+
+console.log("Now, I will be saving these keys before you execute the next step.");
+
+fs.writeFileSync('10_data_vapid_keys.json', stringify(JSONconfig));
