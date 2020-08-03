c4d58a698ba98436a0f25a39a4acc9b0f2abb188
Marcio Galli <taboca@gmail.com>
2020-08-03 11:38:16 -0300

    No end point at this section

--- 20_run_serve_worker.js
+++ 20_run_serve_worker.js
@@ -7,13 +7,6 @@ const configKeys = require('./10_data_vapid_keys.json');
 
 app.use(bodyParser.json());
 
-app.get('/test/vapidPublicKey', function(req, res) {
-  res.send(configKeys['publicKey']);
-  console.log("Returned the public key:");
-  console.log(configKeys['publicKey']);
-  console.log("MANUAL INTERVENTION - stop this script and run 30-serve-register!")
-});
-
 // This will serve the index.html and the index.js which registers the service-worker.js
 app.use('/', express.static(path.join(__dirname, 'public')));
 
