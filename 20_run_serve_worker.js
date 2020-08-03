4ce9a602fbd9903250d74c3c3f64b56ee34e2e67
Marcio Galli <taboca@gmail.com>
2020-08-03 11:36:48 -0300

    Fix print

--- 20_run_serve_worker.js
+++ 20_run_serve_worker.js
@@ -17,4 +17,5 @@ app.get('/test/vapidPublicKey', function(req, res) {
 // This will serve the index.html and the index.js which registers the service-worker.js
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+console.log("Localhost serving at http://localhost:8080");
 app.listen(8080);
